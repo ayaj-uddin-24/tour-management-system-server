4376b7de3a7070bc7a078c8443215b21b461a43a
ayaj-uddin-24 <ayajuddin2024@gmail.com>
2026-01-02 18:55:07 +0600

    Enhance server error handling and add dist to .gitignore

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import { Server } from "http";
 import app from "./app.js";
+
+let server: Server;
 
 const startServer = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/tour-management-server");
     console.log("DB is connected!!!");
 
-    app.listen(5000, () => {
+    server = app.listen(5000, () => {
       console.log("Server is listening on port 5000");
     });
   } catch (error) {
@@ -14,5 +17,49 @@ const startServer = async () => {
   }
 };
 
-
 startServer();
+
+// Unhandled Rejection Error
+process.on("unhandledRejection", (err) => {
+  console.log(
+    "Unhandled Rejection Error Detected. Server Shutting Down!!!",
+    err
+  );
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// Uncaught Exception Error
+process.on("uncaughtException", (err) => {
+  console.log(
+    "Uncaught Exception Error Detected. Server Shutting Down!!!",
+    err
+  );
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// Signal Terminate Error
+process.on("SIGTERM", () => {
+  console.log("Signal Terminate Error Detected. Server Shutting Down!!!");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
