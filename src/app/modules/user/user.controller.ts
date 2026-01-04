a4acd079ed6c9d230d668a1bc6b5520aaa35d081
ayaj-uddin-24 <ayajuddin2024@gmail.com>
2026-01-04 08:09:48 +0600

    Refactor user creation to use catchAsync for error handling

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { userServices } from "./user.service.js";
+import catchAsync from "../../utils/catchAsync.js";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.createUser(req.body);
 
     res
       .status(httpStatus.CREATED)
       .json({ success: true, message: "User Created Successfully!", user });
-  } catch (error: any) {
-    next(error);
   }
-};
+);
 
 export const userController = { createUser };
