ae961b6b35bba48472459099256f765446ed4cae
ayaj-uddin-24 <ayajuddin2024@gmail.com>
2026-01-05 12:25:54 +0600

    Completed user registration with bcryptjs

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,32 @@
-import { IUser } from "./user.interface";
+import { IAuthProviders, IUser } from "./user.interface";
+import AppError from "../../error/AppError";
+import httpStatus from "http-status-codes";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
 
 // Create User Service
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
-  const user = await User.create({ name, email });
+  const { email, password, ...rest } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User already exist!");
+  }
+
+  const hashPassword = await bcryptjs.hash(password as string, 10);
+
+  const authProvider: IAuthProviders = {
+    provider: "credentials",
+    providerId: email as string,
+  };
+
+  const user = await User.create({
+    email,
+    password: hashPassword,
+    auths: [authProvider],
+    ...rest,
+  });
 
   return user;
 };
